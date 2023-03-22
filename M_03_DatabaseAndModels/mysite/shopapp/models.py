3379c10bf504cb7dea2769bd9a9bd4625b6783f5
Вадим Бубнов <vanderlion@rambler.ru>
2023-03-22 13:04:58 +0400

    Upgrade all files

--- M_03_DatabaseAndModels/mysite/shopapp/models.py
+++ M_03_DatabaseAndModels/mysite/shopapp/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 
 class Product(models.Model):
+    class Meta:
+        ordering = ["name", "price"]
+        # db_table = "tech_products"
+        # verbose_name_plural = "products"
+
     name = models.CharField(max_length=100)
     description = models.TextField(null=False, blank=True)
     price = models.DecimalField(default=0, max_digits=8, decimal_places=2)
