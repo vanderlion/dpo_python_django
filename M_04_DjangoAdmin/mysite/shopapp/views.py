9c77aa41ffd7d3ddd9e37fa8609e432e9dc965b9
Вадим Бубнов <vanderlion@rambler.ru>
2023-03-23 15:58:52 +0400

    Download M_04

---
+++ M_04_DjangoAdmin/mysite/shopapp/views.py
@@ -0,0 +1,39 @@
+from timeit import default_timer
+from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render
+from django.contrib.auth.models import Group
+from .models import Product, Order
+
+
+def shop_index(request: HttpRequest):
+    products = [
+        ('Laptop', 1999),
+        ('Desktop', 2999),
+        ('Smartphone', 999),
+    ]
+    context = {
+        "time_running": default_timer(),
+        "products": products,
+    }
+    return render(request, 'shopapp/shop-index.html', context=context)
+
+
+def groups_list(request: HttpRequest):
+    context = {
+        "groups": Group.objects.all(),
+    }
+    return render(request, 'shopapp/groups-list.html', context=context)
+
+
+def products_list(request: HttpRequest):
+    context = {
+        "products": Product.objects.all(),
+    }
+    return render(request, 'shopapp/products-list.html', context=context)
+
+
+def orders_list(request: HttpRequest):
+    context = {
+        "orders": Order.objects.select_related("user").all(),
+    }
+    return render(request, 'shopapp/orders-list.html', context=context)
