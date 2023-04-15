71211f22c66222bc48c96294ad6dac4aba385c7c
Вадим Бубнов <vanderlion@rambler.ru>
2023-04-15 20:44:50 +0400

    add M_11_Files

--- M_11_Files/mysite/myauth/models.py
+++ M_11_Files/mysite/myauth/models.py
@@ -2,7 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+def profile_avatar_directory_path(instance: "Profile", filename: str) -> str:
+    return "account/account_{pk}/avatar/{filename}".format(
+        pk=instance.pk,
+        filename=filename,
+    )
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
-    agreement_accepted = models.BooleanField(default=False)
+    agreement_accept = models.BooleanField(default=False)
+    avatar = models.ImageField(null=True, blank=True, upload_to=profile_avatar_directory_path)
